462f877e73b21a7c7ccfe39f458bd279e0ccd1e7
xuyuantai <17080699@suning.com>
2021-06-25 12:04:00 +0800

    add new api demo

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # YTJsoncpp
-a jsoncpp demo
+
+jsoncpp中主要的类：
+
+- **Json::Value**：可以表示所有支持的类型，如：int , double ,string , object, array等。其包含节点的类型判断(isNull,isBool,isInt,isArray,isMember,isValidIndex等),类型获取(type),类型转换(asInt,asString等),节点获取(get,[]),节点比较(重载<,<=,>,>=,==,!=),节点操作(compare,swap,removeMember,removeindex,append等)等函数。
+- **Json::Reader**：将文件流或字符串创解析到Json::Value中，主要使用parse函数。Json::Reader的构造函数还允许用户使用特性Features来自定义Json的严格等级。
+	- 使用Json::Reader对json文件进行解析
+bool parse(const std::string& document, Value& root, bool collectComments = true);
+参数：document: 包含要读取的文档的UTF-8编码字符串
+root:（输出）Json::Value的对象
+	- 使用Json::Reader对json输入流（文件）进行解析
+bool parse(std:stream& is, Value& root, bool collectComment = true);
+	- 使用Json::Reader对字符串进行解析
+bool parse(const char* beginDoc, const char* ednDoc, Value& root, bool collectComment = true);
+- **Json::Writer**：与JsonReader相反，将Json::Value转换成字符串流等，Writer类是一个纯虚类，并不能直接使用。在此我们使用Json::Writer的子类：Json::FastWriter(将数据写入一行,没有格式，速度快),Json::StyledWriter(按json格式化输出,易于阅读)。
+- JsonCpp其他操作
+	- 判断key是否存在: bool Json::Value::isMember(const char * key) const；存在返回1，否则返回0
+	- 判断是否为null成员函数: bool Json::Value::isNull() const; 和C++中的map有一个共同的特点，就是当你尝试访问一个不存在的key时，会自动生成这样一个key-value默认为null的值对。
+	- 得到所有成员:
+typedef std::vectorstd::string Json::Value::Members;
+Value::Members Json::Value::getMemberNames() const;
+该函数的类型为一个string的vector。
+	- 删除成员: Value Json::Value::removeMember(const char* key) 返回删除的值，或者null
+
+
+Json::Value()常用成员函数
+
+	  String asString() const;
+	  Int asInt() const;
+	  UInt asUInt() const;
+	  Int64 asInt64() const;
+	  UInt64 asUInt64() const;
+	  LargestInt asLargestInt() const;
+	  LargestUInt asLargestUInt() const;
+	  float asFloat() const;
+	  double asDouble() const;
+	  bool asBool() const;
+
+	  bool isNull() const;
+	  bool isBool() const;
+	  bool isInt() const;
+	  bool isInt64() const;
+	  bool isUInt() const;
+	  bool isUInt64() const;
+	  bool isIntegral() const;
+	  bool isDouble() const;
+	  bool isNumeric() const;
+	  bool isString() const;
+	  bool isArray() const;
+	  bool isObject() const;
+
+	enum ValueType {
+	  nullValue = 0, ///< 'null' value
+	  intValue,      ///< signed integer value
+	  uintValue,     ///< unsigned integer value
+	  realValue,     ///< double value
+	  stringValue,   ///< UTF-8 string value
+	  booleanValue,  ///< bool value
+	  arrayValue,    ///< array value (ordered list)
+	  objectValue    ///< object value (collection of name/value pairs).
+	};
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 
+// 解决旧API编译错误问题
 #if defined(__GNUC__)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
@@ -12,21 +13,21 @@
 
 #include "json/json.h"
 
-int main(int argc, char *argv[])
+void OldAPIDemo()
 {
     // old api writer
     {
         Json::Value root;
         Json::Value person;
 
-        // [{"age":35,"name":"Tellen"}]
+        // [{"age":35,"name":"Tellen","school":"nuaa"}]
         person["name"] = "Tellen";
         person["age"] = 35;
         person["school"] = "nuaa";
         root.append(person);
 
-        Json::FastWriter writer;
-        std::string json_no_style = writer.write(root);
+        Json::FastWriter fast_writer;
+        std::string json_no_style = fast_writer.write(root);
         std::cout << "~~~~~~Json::FastWriter:" << std::endl;
         std::cout << json_no_style << std::endl;
 
@@ -46,6 +47,8 @@ int main(int argc, char *argv[])
         styled_stream_writer.write(std::cout, root);
         styled_stream_writer.write(ofs, root);
         ofs.close();
+
+        std::cout << "toStyledString:\n" << root.toStyledString();
     }
 
     // old api reader
@@ -53,14 +56,14 @@ int main(int argc, char *argv[])
         std::ifstream ifs;
         ifs.open("test.json");
         assert(ifs.is_open());
-        
+
         Json::Reader reader;
         Json::Value root;
         if (!reader.parse(ifs, root, false))
         {
             std::cout << "parse error" << std::endl;
             ifs.close();
-            return -1;
+            return;
         }
         ifs.close();
 
@@ -89,6 +92,115 @@ int main(int argc, char *argv[])
             }
         }
     }
+}
+
+void CreatJsonNewAPI()
+{
+    Json::Value root;
+    Json::Value lang;
+    Json::Value work;
+
+    root["name"] = "tellen";
+    root["age"] = 35;
+
+    lang[0] = "C++";
+    lang[1] = "python";
+    lang[2] = "java";
+    root["language"] = lang;
+
+    work["work1"] = "nrist";
+    work["work2"] = "palmap";
+    work["work3"] = "sn";
+    root["work"] = work;
+
+    Json::StreamWriterBuilder writerBuilder;
+    std::ostringstream oss;
+    //writerBuilder.settings_["indentation"] = ""; // 非格式化输出
+    std::unique_ptr<Json::StreamWriter> jsonWriter(writerBuilder.newStreamWriter());
+    jsonWriter->write(root, &oss);
+    std::cout << "~~~~~~New API:\n"
+              << oss.str() << std::endl;
+
+    std::cout << "~~~~~~std::cout:" << std::endl;
+    jsonWriter->write(root, &std::cout);
+}
+
+bool ParseJsonNewAPI(const std::string &str_json)
+{
+    if (str_json.empty())
+    {
+        std::cout << "~~~~~~ParseJsonNewAPI error input null" << std::endl;
+        return false;
+    }
+
+    bool res;
+    JSONCPP_STRING errs;
+    Json::Value root;
+    Json::Value lang;
+    Json::Value work;
+    Json::CharReaderBuilder readerBuilder;
+
+    std::unique_ptr<Json::CharReader> const jsonReader(readerBuilder.newCharReader());
+    res = jsonReader->parse(str_json.c_str(), str_json.c_str() + str_json.length(), &root, &errs);
+    if (!res || !errs.empty())
+    {
+        std::cout << "~~~~~~ParseJsonNewAPI error. " << errs << std::endl;
+        return false;
+    }
+
+    std::cout << "~~~~~~ParseJsonNewAPI:" << std::endl;
+    std::cout << "name: " << root["name"].asString() << std::endl;
+    std::cout << "age: " << root["age"].asInt() << std::endl;
+
+    lang = root["language"];
+    std::cout << "language: ";
+    for (int i = 0; i < lang.size(); ++i)
+    {
+        std::cout << lang[i] << " ";
+    }
+    std::cout << std::endl;
+
+    work = root["work"];
+    std::cout << "work1: " << work["work1"].asString() << std::endl;
+    std::cout << "work2: " << work["work2"].asString() << std::endl;
+    std::cout << "work3: " << work["work3"].asString() << std::endl;
+
+    // 判断key是否存在
+    bool is_age = root.isMember("age");
+    std::cout << "isMember:" << std::boolalpha << is_age << std::endl;
+
+    // 得到所有的key
+    std::cout << "keys: ";
+    for (auto elem : root.getMemberNames())
+    {
+        std::cout << elem << " ";
+    }
+    std::cout << std::endl;
+
+    // 删除成员
+    root.removeMember("age");
+
+    // 得到所有的key
+    std::cout << "keys: ";
+    for (auto elem : root.getMemberNames())
+    {
+        std::cout << elem << " ";
+    }
+    std::cout << std::endl;
+
+    return true;
+}
+
+int main(int argc, char *argv[])
+{
+    OldAPIDemo();
+
+    CreatJsonNewAPI();
+
+    std::string str_json = "{\"age\":35,\"language\":[\"C++\",\"python\",\"java\"],\"name\":\"tellen\",\"work\":{\"work1\":\"nrist\",\"work2\":\"palmap\",\"work3\":\"sn\"}}";
+    ParseJsonNewAPI(str_json);
+    str_json = "abc";
+    ParseJsonNewAPI(str_json);
 
     return 0;
 }
